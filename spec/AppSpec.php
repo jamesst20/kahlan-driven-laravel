28ed77a9e780a67836002c6ad8a03dedb1396bfa
James St-Pierre <jamesst20@gmail.com>
2019-03-06 20:48:25 -0500

    Laravel 5.8

--- spec/AppSpec.php
+++ spec/AppSpec.php
@@ -3,12 +3,11 @@
 use Illuminate\Contracts\Foundation\Application;
 
 describe('Laravel context for kahlan specs', function () {
-
     // Let's (re)create the DB as starting point,
     // because it gets wiped at the end, when
     // we use database migrations wrappers.
-    before(function () {
-        $this->laravel->artisan('migrate');
+    beforeEach(function () {
+        $this->laravel->artisan('migrate:fresh');
     });
 
 
@@ -56,21 +55,27 @@ describe('Laravel context for kahlan specs', function () {
     context('It provides the same testing API as laravel TestCase', function () {
         it('crawls & asserts', function () {
             $this->laravel->get('/')
-                          ->see('Laravel')
-                          ->see('Documentation')
-                          ->assertResponseOk();
+                          ->assertSee('Laravel')
+                          ->assertSee('Docs')
+                          ->assertSee('Laracast')
+                          ->assertSee('News')
+                          ->assertSee('Blog')
+                          ->assertSee('Nova')
+                          ->assertSee('Forge')
+                          ->assertSee('GitHub')
+                          ->assertSuccessful();
         });
 
         it('interacts with database', function () {
             factory(App\User::class)->create(['email' => 'test@email.com']);
-            $this->laravel->seeInDatabase('users', ['email' => 'test@email.com']);
+            $this->laravel->assertDatabaseHas('users', ['email' => 'test@email.com']);
         });
 
         it('interacts with session', function () {
             $this->laravel->withSession(['session_test' => 'working'])
                           ->get('/session-test')
-                          ->see('working')
-                          ->seeInSession(['session_test' => 'working']);
+                          ->assertSee('working')
+                          ->assertSessionHas('session_test','working');
         });
 
         it('interacts with app services', function () {
@@ -79,67 +84,6 @@ describe('Laravel context for kahlan specs', function () {
 
             event('event_one');
             event('event_two');
-        });
-    });
-
-    /*
-    |--------------------------------------------------------------------------
-    | In order to use functionality provided by Laravel testing traits
-    | eg. `use DatabaseTransactions` simply wrap your specs context
-    | provided by `using()` and its alias `wrapEach()` functions.
-    |
-    | For example to add DatabaseTransactions you can use any of:
-    |  - 'database.transactions' // dot notation
-    |  - 'DatabaseTransactions'  // original laravel trait name
-    |  - 'database transactions' // simple, human-readable string
-    |  - 'database transaction'  // any of the above in SINGULAR form
-    |
-    |--------------------------------------------------------------------------
-    */
-    using(['database transactions'], function () {
-
-        it('can wrap db operations in transaction', function () {
-            factory(App\User::class)->create(['email' => 'some@email.com']);
-            expect(App\User::where('email', 'some@email.com')->exists())->toBe(true);
-        });
-
-        it('then rolls back transaction', function () {
-            expect(App\User::where('email', 'some@email.com')->exists())->toBe(false);
-        });
-    });
-
-    using('without middleware', function () {
-        it('runs without middleware on demand (auth)', function () {
-            $this->laravel
-                 ->get('/admin')
-                 ->see('admin area for logged in user only');
-        });
-
-        it('runs without middleware on demand (guest)', function () {
-            $this->laravel
-                 ->actingAs(factory(App\User::class)->create())
-                 ->get('/login')
-                 ->see('login form for guests only');
-        });
-    });
-
-    using('without events', function () {
-        it('runs without events on demand', function () {
-            app('events')->listen('test_event', function () {
-                return 'fired';
-            });
-            expect(event('test_event'))->toBe(null);
-        });
-    });
-
-    using(['database migrations'], function () {
-        it('can migrate db for...', function () {
-            factory(App\User::class)->create(['email' => 'some@email.com']);
-            expect(App\User::where('email', 'some@email.com')->exists())->toBe(true);
-        });
-
-        it('...each single spec', function () {
-            expect(App\User::where('email', 'some@email.com')->exists())->toBe(false);
         });
     });
 
